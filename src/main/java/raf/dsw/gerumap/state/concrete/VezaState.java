4f6d6d56b80519d589fa5e9ab51993f154f73d7f
Ignjat Krdzavac <ikrdzavac11520rn@raf.rs>
2022-12-05 01:48:29 +0100

    Onemoguceno pravljenje vise veza za dva ista pojma

--- src/main/java/raf/dsw/gerumap/state/concrete/VezaState.java
+++ src/main/java/raf/dsw/gerumap/state/concrete/VezaState.java
@@ -2,6 +2,7 @@ package raf.dsw.gerumap.state.concrete;
 
 import raf.dsw.gerumap.gui.swing.painter.ElementPainter;
 import raf.dsw.gerumap.gui.swing.painter.PojamPainter;
+import raf.dsw.gerumap.gui.swing.painter.VezaPainter;
 import raf.dsw.gerumap.gui.swing.tree.model.MapTreeItem;
 import raf.dsw.gerumap.gui.swing.view.MindMapPanel;
 import raf.dsw.gerumap.mapRepository.implementation.MindMap;
@@ -12,16 +13,26 @@ import raf.dsw.gerumap.observer.ISubscriber;
 import raf.dsw.gerumap.state.State;
 
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class VezaState implements State {
 
     PojamElement odPojma;
     PojamElement doPojma;
 
+     private List<VezaElement> listaVeza = new ArrayList<>();
+
+     private  boolean postoji;
+
     @Override
     public void mousePressed(int x, int y, MindMap mindMap, MapTreeItem parent) {
         List<ISubscriber> listaSubova =  mindMap.getSubscribers();
+        odPojma = null;
+        doPojma = null;
+        postoji = false;
 
         for(ISubscriber subscriber:listaSubova){
 
@@ -79,10 +90,19 @@ public class VezaState implements State {
 
 
 
-
-        VezaElement vezaElement = new VezaElement(odPojma,doPojma);
-        mindMap.addChild(vezaElement);
-        mindMap.notifyObs(vezaElement, parent);
+        if((odPojma != null) && (doPojma != null)) {
+            for(VezaElement vezel:listaVeza){
+                if(vezel.getOdPojma().equals(odPojma) && vezel.getDoPojma().equals(doPojma) || vezel.getOdPojma().equals(doPojma) && vezel.getDoPojma().equals(odPojma)){
+                    postoji = true;
+                }
+            }
+                if(!postoji) {
+                    VezaElement vezaElement = new VezaElement(odPojma, doPojma);
+                    listaVeza.add(vezaElement);
+                    mindMap.addChild(vezaElement);
+                    mindMap.notifyObs(vezaElement, parent);
+                }
+        }
 
     }
 }
